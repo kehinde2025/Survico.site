a4652f93da843d106e308285f09d46ab49d5bc20
kehinde2025 <oyetoyinbokehinde19@mail.com>
2025-09-24 18:27:21 +0100

    Updates

---
+++ assets/axios-TdUo7yJ5.js
@@ -0,0 +1,1 @@
+import{a}from"./index-Dq7h7Pqt.js";const p=a.create({baseURL:"https://6ac75eda3ac5.ngrok-free.app/api",headers:{"Content-Type":"application/json"}});export{p as a};
--- assets/axios-dF6Nnch2.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./index-Dq7h7Pqt.js";const p=a.create({baseURL:"https://survico-backend-production.up.railway.app"});export{p as a};
